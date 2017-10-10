7c3a20709064ae3a219b07e798a7f6f8bd068e9f
Christoph Düsing <christophduesing97@gmail.com>
2017-10-10 22:30:53 +0200

    added IOManager for test purpose

---
+++ WIP/src/main/IOManager.java
@@ -0,0 +1,29 @@
+package main;
+import java.sql.*;
+
+//https://jaxenter.de/mysql-und-java-jdbc-einrichten-8180
+//Da steht hoffentlich alles
+
+public class IOManager{
+	Connection connection;
+	
+	public IOManager() {
+	 
+	}
+	
+	public String test() {
+		return "test";
+	}
+	 
+	public boolean connectToMysql(String host, String database, String user, String passwd) {
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String connectionCommand = "jdbc:mysql://"+host+"/"+database+"?user="+user+"&password="+passwd;
+			connection = DriverManager.getConnection(connectionCommand);
+			return true;
+		} catch (Exception ex) {
+			System.out.println("false");
+			return false;
+		}
+	}
+}
